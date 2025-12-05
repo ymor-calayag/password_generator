4937770d2bb0658c278a73f5d3871fc6e0409c67
ymor-calayag <jeffreyymor.calayag@gmail.com>
2025-12-06 00:49:06 +0800

    refactor password generator to avoid the password list to be a global variable

--- main.py
+++ main.py
@@ -3,25 +3,26 @@ import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-password = []
 
 def main():
+    password = []
+
     print("Welcome to the PyPassword Generator!")
     print("---------------")
     num_of_letters = int(input("How many letters would you like in your password?\n"))
-    generate_characters(num_of_letters, letters)
+    generate_characters(num_of_letters, letters, password)
 
     num_of_symbols = int(input(f"How many symbols would you like?\n"))
-    generate_characters(num_of_symbols, symbols)
+    generate_characters(num_of_symbols, symbols, password)
 
     num_of_numbers = int(input(f"How many numbers would you like?\n"))
-    generate_characters(num_of_numbers, numbers)
+    generate_characters(num_of_numbers, numbers, password)
 
     print("---------------")
     random.shuffle(password)
     print(f"Your password is: {''.join(password)}")
 
-def generate_characters(char_num, char_list):
+def generate_characters(char_num, char_list, password):
     count = 0
     while True:
         rand_index = random.randint(0, len(char_list) - 1)
